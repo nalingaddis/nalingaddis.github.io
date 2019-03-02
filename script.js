fb6c1183b893bcc4f8be9d9bbf9aeb55e1f331f3
nalingaddis <nalin.gaddis@gmail.com>
2019-03-01 22:56:14 -0500

    Search bar makes the API call

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-var searchBar = document.getElementById("searchBar");
+//Elements on the page
+let searchButton = document.getElementById("searchButton")
+let searchBar = document.getElementById("searchBar");
 
-function search(event){
+//Allows the user to search with the "enter" key
+searchBar.addEventListener("keyup",function(event){
   if(event.keyCode === 13){
-    event.preventDefault();
-    document.getElementById("searchButton").click();
+    searchButton.click();
   }
+});
+
+//NASA's API addresses
+const rootAddress = "https://images-api.nasa.gov"
+const searchExt = "/search"
+
+//Creating an XMLHttpRequest object
+const request = new XMLHttpRequest();
+
+//Defining behavior upon request response
+request.onreadystatechange = function(){
+  if(request.readyState === 4){
+    if(request.status === 200){
+       console.log(request.responseText);
+    } else {
+      alert("API did not respond as expected");
+    }
+  }
+};
+
+//Calls NASA's Search API, uses seachBar's text for q
+function callSearchAPI(q){
+  request.open("GET",rootAddress+searchExt+"?q="+q);
+  request.send();
 }
 
-searchBar.addEventListener("keyup",search(event))
+//Handles input once search button is pressed
+function search(){
+  var text = searchBar.value;
+  if(text != ""){
+    callSearchAPI(text);
+  }
+}
