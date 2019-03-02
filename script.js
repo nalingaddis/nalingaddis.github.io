3e634144adeddd51943caba73f31b39c927a5070
nalingaddis <nalin.gaddis@gmail.com>
2019-03-02 14:01:32 -0500

    display search results count

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/************* Initialize Search Elements On the Page  *************/
 //Elements on the page
 const searchButton = document.getElementById("searchButton");
   //Array of Input Fields
@@ -6,13 +7,14 @@ const inputFields = document.querySelectorAll(".inputField")
 //Allows the user to search with the "enter" key
 inputFields.forEach(function(elem){
   elem.addEventListener("keyup",function(event){
-    searchButton.click();
+    if(event.keyCode === 13){searchButton.click();}
   });
-})
+});
 
+/************* Making API Calls  *************/
 //NASA's API addresses
 const rootAddress = "https://images-api.nasa.gov"
-const searchExt = "/search"
+const searchExt = "/search?media_type=image&" //always return images
 
 //Creating an XMLHttpRequest object and response object
 const request = new XMLHttpRequest();
@@ -22,7 +24,7 @@ let response;
 request.onreadystatechange = function(){
   if(request.readyState == 4){
     if(request.status == 200){
-       response = JSON.parse(request.responseText);
+       processResponse(request.responseText);
     } else if(request.status == 400){
       alert("Bad Request");
     } else if(request.state == 404){
@@ -35,14 +37,29 @@ request.onreadystatechange = function(){
 
 //Calls NASA's Search API, uses seachBar's text for q
 function callSearchAPI(q){
-  request.open("GET",rootAddress+searchExt+"?q="+q);
+  request.open("GET",buildSearchRequest());
   request.send();
 }
 
-//Handles input once search button is pressed
-function search(){
-  var text = inputFields[0].value;
-  if(text != ""){
-    callSearchAPI(text);
-  }
+//Creates the Address for API call
+function buildSearchRequest(){
+  let ext = [];
+  inputFields.forEach(function(elem){
+    if(elem.value !== ""){
+      ext.push(encodeURIComponent(elem.id) + "=" + encodeURIComponent(elem.value));
+    }
+  });
+  //Joins the root, search, and query
+  return rootAddress+searchExt+ext.join("&");;
+}
+
+/************* Procesing API Response's  *************/
+function processResponse(responseText){
+  response = JSON.parse(responseText);
+  console.log(response);
+  
+  let imagePane = document.getElementById("imagePane");
+
+  let searchCount = document.getElementById("searchCount");
+  searchCount.textContent = response.collection.metadata["total_hits"] + " search results.";
 }
